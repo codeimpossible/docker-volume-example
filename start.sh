3875d85b91d6e972c6c1642c20ccdba36bce288e
Jared Barboza <codeimpossible@users.noreply.github.com>
2018-08-28 12:47:34 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# run this file on your local machine to kick off the example
+. ./vars.sh
+
+set -e
+
+docker build -f Prep.dockerfile -t prep .
+docker run \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    prep
